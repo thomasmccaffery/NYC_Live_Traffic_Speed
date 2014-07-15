d7aff776c8b79fc093f6d15a7a39d2825bf0b8a9
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-15 01:25:41 -0400

    Debug-Comment Version of Data_Pull.php
    Extended version of Data_Pull.php; Pulls data from NYCDOT API, and
    leaves in all debugging and comments to see the different stages of
    conversion from txt file to SQL input.

---
+++ Data_Pull_Debug.php
@@ -0,0 +1,48 @@
+<?
+
+include_once 'inc/connection.php';   /* DB Connection */
+$Time_Check = mysqli_query($con,"SELECT `DataAsOf` FROM Traffic_Speed ORDER BY `DataAsOf` DESC LIMIT 1");
+$Timed = mysqli_fetch_array($Time_Check);
+
+/* Pull Data From URL */
+function get_data($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+$Raw_NYC_Traffic_Speed = get_data('http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt');
+
+/*print_r($Raw_NYC_Traffic_Speed);*/ /* Prints the entire txt file, for testing purposes. */
+
+
+$Data_Rows = explode("\n", $Raw_NYC_Traffic_Speed);
+unset($Data_Rows[0]);
+foreach ($Data_Rows as $New_Traffic_Data) {
+	if($New_Traffic_Data) {
+		// echo "$New_Traffic_Data <br>===========<br>"; /* Outputs Raw Row Data for each new entry. */
+
+		$Individual_Columns = str_getcsv($New_Traffic_Data, "	", "\""); /* Breaks down data into individual columns separated by tabs between data in quotation. */
+		
+		if(($Individual_Columns[4] > $Timed['DataAsOf'])) { /* find out which ID is unique and add an extra check! */
+
+			foreach ($Individual_Columns as $Data_Columns) {
+				// echo $Data_Columns." | "; /* Outputs Each piece of data separated by a line for visual testing. */
+			}
+			echo $Individual_Columns[4]; /* Outputs Data of current row. (Used later to check if in DB) */
+			echo ":::";
+			echo $Timed['DataAsOf']; /* Outputs Most recent time which loop is checked against. */
+			
+			mysqli_query($con,"INSERT INTO Traffic_Speed (Id, Speed, TravelTime, Status, DataAsOf, linkId, linkPoints, EncodedPolyLine, EncodedPolyLineLvls, Owner, Transcom_id, Borough, linkName) VALUES ($Individual_Columns[0], $Individual_Columns[1], $Individual_Columns[2], $Individual_Columns[3], '$Individual_Columns[4]', $Individual_Columns[5], '$Individual_Columns[6]', '$Individual_Columns[7]', '$Individual_Columns[8]', '$Individual_Columns[9]', $Individual_Columns[10], '$Individual_Columns[11]', '$Individual_Columns[12]')"); /* INSERT New data into the DB. */
+			
+			echo "<br/>----- New Entry -----<br/>";
+		}
+	}
+}
+
+?>
