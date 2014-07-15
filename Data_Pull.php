6e8430a034f1bddf278e566f367175567c85fce8
thomasmccaffery <tom@thomasmccaffery.com>
2014-07-15 17:42:32 -0400

    Major FIxes
    Fixed an issue where the time was being checked as a string, thus
    incorrectly adding times.
    Fixed an issue where false sensor readings, and incorrect dates would
    appear, filtered them out in the if statement.
    Fixed an issue where some data rows would be split because the NYCDOT
    decided to throw in line-breaks into random locations.

--- Data_Pull.php
+++ Data_Pull.php
@@ -2,7 +2,8 @@
 
 include_once 'inc/connection.php';   /* DB Connection */
 $Time_Check = mysqli_query($con,"SELECT `DataAsOf` FROM Traffic_Speed ORDER BY `DataAsOf` DESC LIMIT 1");
-$Timed = mysqli_fetch_array($Time_Check);
+$Timed = mysqli_fetch_array($Time_Check); /* Most Recent Date Imported to the DB. */
+$Date_Today = date("Y-m-d"); /* Today's Date. */
 
 /* Pull Data From URL */
 function get_data($url) {
@@ -18,15 +19,18 @@ function get_data($url) {
 
 $Raw_NYC_Traffic_Speed = get_data('http://207.251.86.229/nyc-links-cams/LinkSpeedQuery.txt');
 
-$Data_Rows = explode("\n", $Raw_NYC_Traffic_Speed);
-unset($Data_Rows[0]);
+$Raw_NYC_Traffic_Speed = preg_replace('#\\n(?=[^"]*"[^"]*(?:"[^"]*"[^"]*)*$)#' , ' ', $Raw_NYC_Traffic_Speed); /* Removes Line-Breaks '\n' from raw feed, which splits some rows when imported due to poor API Feed Programming by NYCDOT */
+$Data_Rows = explode("\n", $Raw_NYC_Traffic_Speed); /* Splits Feed into rows for each speed entry. */
+unset($Data_Rows[0]); /* Removes the Feed Data Headers */
+
 foreach ($Data_Rows as $New_Traffic_Data) {
 	if($New_Traffic_Data) {
-
-		$Individual_Columns = str_getcsv($New_Traffic_Data, "	", "\""); /* Breaks down data into individual columns separated by tabs between data in quotation. */
+		$Individual_Columns = str_getcsv($New_Traffic_Data, "	", "\""); /* Breaks down data into individual columns separated by tabs between data in quotation. */		
+		$timestamp = strtotime($Individual_Columns[4]);
+		$timestamp = date("Y-m-d H:i:s", $timestamp); /* Convert Date & Time to TIMEDATE for MySQL ordering. */
 		
-		if(($Individual_Columns[4] > $Timed['DataAsOf'])) { /* find out which ID is unique and add an extra check! */
-			mysqli_query($con,"INSERT INTO Traffic_Speed (Id, Speed, TravelTime, Status, DataAsOf, linkId, linkPoints, EncodedPolyLine, EncodedPolyLineLvls, Owner, Transcom_id, Borough, linkName) VALUES ($Individual_Columns[0], $Individual_Columns[1], $Individual_Columns[2], $Individual_Columns[3], '$Individual_Columns[4]', $Individual_Columns[5], '$Individual_Columns[6]', '$Individual_Columns[7]', '$Individual_Columns[8]', '$Individual_Columns[9]', $Individual_Columns[10], '$Individual_Columns[11]', '$Individual_Columns[12]')"); /* INSERT New data into the DB. */			
+		if(($Individual_Columns) && ($Date_Today<=$timestamp) && ($timestamp > $Timed['DataAsOf']) && ($Individual_Columns[3]==0)) { /* find out which ID is unique and add an extra check! */
+			mysqli_query($con,"INSERT INTO Traffic_Speed (Id, Speed, TravelTime, Status, DataAsOf, linkId, linkPoints, EncodedPolyLine, EncodedPolyLineLvls, Owner, Transcom_id, Borough, linkName) VALUES ($Individual_Columns[0], $Individual_Columns[1], $Individual_Columns[2], $Individual_Columns[3], '$timestamp', $Individual_Columns[5], '$Individual_Columns[6]', '$Individual_Columns[7]', '$Individual_Columns[8]', '$Individual_Columns[9]', $Individual_Columns[10], '$Individual_Columns[11]', '$Individual_Columns[12]')"); /* INSERT New data into the DB. */
 		}
 	}
 }
